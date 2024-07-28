fc3688fb9a138c5ba937fd814f8fbe05d0b88f28
Yader Campbell <yadermejiac123@gmail.com>
2024-07-27 23:05:18 -0600

    feat: Login design, static widgets

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,8 +7,91 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text("LoginPage"),
+      backgroundColor: Color(0xffF2F2F2),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _Logo(),
+            _Form(),
+            _Labels(),
+
+            Text(
+              "Terminos y condiciones de uso", 
+              style: TextStyle(color: Colors.black45, fontSize: 12, fontWeight: FontWeight.w200),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 170,
+        margin: const EdgeInsets.only(top: 50.0),
+        child: const Column(
+          children: [
+            Image(image: AssetImage('assets/tag-logo.png')),
+            SizedBox(height: 20.0),
+            Text("Messenger", style: TextStyle(fontSize: 30))
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class _Form extends StatefulWidget {
+  const _Form();
+
+  @override
+  State<_Form> createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          const TextField(),
+          const TextField(),
+
+          ElevatedButton(
+            onPressed: (){}, 
+            child: const Text("Login"),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Labels extends StatelessWidget {
+  const _Labels({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          const Text(
+            "No tienes cuenta?", 
+            style: TextStyle(color: Colors.black54, fontSize: 15, fontWeight: FontWeight.w300),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            "Crear una ahora!", 
+            style: TextStyle(color: Colors.blue.shade600, fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ],
       ),
     );
   }
