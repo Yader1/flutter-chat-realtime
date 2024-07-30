d7a7e8fd5bc8f04eae6229b3ac67fa7153707d83
Yader Campbell <yadermejiac123@gmail.com>
2024-07-29 22:26:21 -0600

    feat: Specific appBar and list chat design

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -5,10 +5,40 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("ChatPage"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Column(
+          children: [
+            CircleAvatar(
+              child: Text("Ya", style: TextStyle(fontSize: 12)),
+              backgroundColor: Colors.blue.shade100,
+            ),
+            SizedBox(height: 3),
+            Text("Yader Mejia", style: TextStyle(color: Colors.black87, fontSize: 14))
+          ],
+        ),
+        centerTitle: true,
+        elevation: 1,
       ),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (_, i) => Text("$i"),
+                reverse: true
+              )
+            ),
+            const Divider(height: 1),
+            Container(
+              color: Colors.white,
+              height: 50,
+            )
+          ],
+        ),
+      )
     );
   }
 }
