b9c36a014ed2d1f5679de553677e14cbb0179c51
Yader Campbell <yadermejiac123@gmail.com>
2024-07-29 23:17:35 -0600

    feat: Chat text box

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,7 +1,18 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _textController = TextEditingController();
+  final _focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
@@ -34,11 +45,54 @@ class ChatPage extends StatelessWidget {
             const Divider(height: 1),
             Container(
               color: Colors.white,
-              height: 50,
+              child: _inputChat(),
             )
           ],
         ),
       )
     );
+  }
+
+  Widget _inputChat(){
+    return SafeArea(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmit,
+                onChanged: (String texto){
+
+                },
+                decoration: const InputDecoration.collapsed(
+                  hintText: "Enviar mensaje",
+                ),
+                focusNode: _focusNode,
+              ) 
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: Platform.isIOS ? CupertinoButton(
+                child: const Text('Enviar'), 
+                onPressed: (){}
+              ) : Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: IconButton(
+                  onPressed: (){}, 
+                  icon: Icon(Icons.send, color: Colors.blue.shade400)
+                ),
+              ),
+            )
+          ],
+        ),
+      ) 
+    );
+  }
+
+  _handleSubmit(String texto){
+    _focusNode.requestFocus();
+    _textController.clear();
   }
 }
